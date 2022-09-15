62564f0737257fb65819ea49a5df8d287844496b
Filipe Paulino <filipepaulinodeveloper@hotmail.com>
2022-09-15 05:15:17 -0300

    Tela de cadastro de livro

--- src/views/lista-e-cadastro/Livro/Cad-livro.vue
+++ src/views/lista-e-cadastro/Livro/Cad-livro.vue
@@ -1,331 +1,161 @@
+ 
 <template>
-  <div>
-    <form-wizard
-      color="#7367F0"
-      :title="null"
-      :subtitle="null"
-      layout="vertical"
-      finish-button-text="Submit"
-      back-button-text="Previous"
-      class="wizard-vertical mb-3"
-      @on-complete="formSubmitted"
-    >
+    <div>
+      <b-card>
+          <b-form @submit.prevent>
+    <b-row>
+      <b-col md="6">
+        <b-form-group
+          label="First Name"
+          label-for="mc-first-name"
+        >
+          <b-form-input
+            id="mc-first-name"
+            placeholder="First Name"
+          />
+        </b-form-group>
+      </b-col>
+      <b-col md="6">
+        <b-form-group
+          label="Last Name"
+          label-for="mc-last-name"
+        >
+          <b-form-input
+            id="mc-last-name"
+            placeholder="Last Name"
+          />
+        </b-form-group>
+      </b-col>
+      <b-col md="6">
+        <b-form-group
+          label="City"
+          label-for="mc-city"
+        >
+          <b-form-input
+            id="mc-city"
+            placeholder="City"
+          />
+        </b-form-group>
+      </b-col>
+      <b-col md="6">
+        <b-form-group
+          label="Country"
+          label-for="mc-country"
+        >
+          <b-form-input
+            id="mc-country"
+            placeholder="Country"
+          />
+        </b-form-group>
+      </b-col>
+      <b-col md="6">
+        <b-form-group
+          label="Company"
+          label-for="mc-company"
+        >
+          <div class="form-label-group">
+            <b-form-input
+              id="mc-company"
+              placeholder="Company"
+            />
+          </div>
+        </b-form-group>
+      </b-col>
+      <b-col md="6">
+        <b-form-group
+          label-for="mc-email"
+          label="Email"
+        >
+          <div class="form-label-group">
+            <b-form-input
+              id="mc-email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+        </b-form-group>
+      </b-col>
 
-      <!-- account datails tab -->
-      <tab-content title="Account Details">
-        <b-row>
-          <b-col
-            cols="12"
-            class="mb-2"
-          >
-            <h5 class="mb-0">
-              Account Details
-            </h5>
-            <small class="text-muted">
-              Enter Your Account Details.
-            </small>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Username"
-              label-for="v-username"
-            >
-              <b-form-input
-                id="v-username"
-                placeholder="johndoe"
+      <b-col md="6">
+        <b-form-group
+          label-for="textarea-no-resize"
+          label="textarea-no-resize"
+        >
+           <div>
+              <b-form-textarea
+                id="textarea-no-resize"
+                placeholder="Fixed height textarea"
+                rows="3"
+                no-resize
               />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Email"
-              label-for="v-email"
-            >
-              <b-form-input
-                id="v-email"
-                type="email"
-                placeholder="john.doe@email.com"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Password"
-              label-for="v-password"
-            >
-              <b-form-input
-                id="v-password"
-                type="password"
-                placeholder="Password"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Confirm Password"
-              label-for="v-c-password"
-            >
-              <b-form-input
-                id="v-c-password"
-                type="password"
-                placeholder="Re-type Password"
-              />
-            </b-form-group>
-          </b-col>
-        </b-row>
-      </tab-content>
+            </div>
+        </b-form-group>
+      </b-col>
+       <b-col md="6">
+        <b-form-group
+          label-for="wildcard"
+          label="wildcard"
+        >
+            
+             
+      <b-form-file
+        id="wildcard"
+        accept="image/*"
+      />
+        </b-form-group>
+      </b-col>
 
-      <!-- personal info tab -->
-      <tab-content title="Personal Info">
-        <b-row>
-          <b-col
-            cols="12"
-            class="mb-2"
-          >
-            <h5 class="mb-0">
-              Personal Info
-            </h5>
-            <small class="text-muted">Enter Your Personal Info.</small>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="First Name"
-              label-for="v-first-name"
-            >
 
-              <b-form-input
-                id="v-first-name"
-                placeholder="John"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Last Name"
-              label-for="v-last-name"
-            >
-              <b-form-input
-                id="v-last-name"
-                placeholder="Doe"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Country"
-              label-for="v-country"
-            >
-              <v-select
-                id="v-country"
-                v-model="selectedContry"
-                :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"
-                :options="countryName"
-                :selectable="option => ! option.value.includes('select_value')"
-                label="text"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label-for="v-language"
-              label="Language"
-            >
-              <v-select
-                id="Language"
-                v-model="selectedLanguage"
-                :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"
-                :selectable="option => ! option.value.includes('select_value')"
-                :options="languageName"
-                label="text"
-              />
-            </b-form-group>
-          </b-col>
-        </b-row>
-      </tab-content>
 
-      <!-- address -->
-      <tab-content title="Address">
-        <b-row>
-          <b-col
-            cols="12"
-            class="mb-2"
-          >
-            <h5 class="mb-0">
-              Address
-            </h5>
-            <small class="text-muted">Enter Your Address.</small>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Address"
-              label-for="v-address"
-            >
-              <b-form-input
-                id="v-address"
-                placeholder="98 Borough bridge Road, Birmingham"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Landmark"
-              label-for="v-landmark"
-            >
-              <b-form-input
-                id="v-landmark"
-                placeholder="Borough bridge"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Pincode"
-              label-for="v-pincode"
-            >
-              <b-form-input
-                id="v-pincode"
-                placeholder="658921"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="City"
-              label-for="v-city"
-            >
-              <b-form-input
-                id="v-city"
-                placeholder="Birmingham"
-              />
-            </b-form-group>
-          </b-col>
-        </b-row>
-      </tab-content>
-
-      <!-- social link -->
-      <tab-content title="Social Links">
-        <b-row>
-          <b-col
-            cols="12"
-            class="mb-2"
-          >
-            <h5 class="mb-0">
-              Social Links
-            </h5>
-            <small class="text-muted">Enter Your Social Links</small>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Twitter"
-              label-for="v-twitter"
-            >
-              <b-form-input
-                id="v-twitter"
-                placeholder="https://twitter.com/abc"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Facebook"
-              label-for="v-facebook"
-            >
-              <b-form-input
-                id="v-facebook"
-                placeholder="https://facebook.com/abc"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="Google+"
-              label-for="v-google-plus"
-            >
-              <b-form-input
-                id="v-google-plus"
-                placeholder="https://plus.google.com/abc"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col md="6">
-            <b-form-group
-              label="LinkedIn"
-              label-for="v-linked-in"
-            >
-              <b-form-input
-                id="v-linked-in"
-                placeholder="https://linkedin.com/abc"
-              />
-            </b-form-group>
-          </b-col>
-        </b-row>
-      </tab-content>
-    </form-wizard>
-
-  </div>
+      <!-- submit and reset -->
+      <b-col>
+        <b-button
+          v-ripple.400="'rgba(255, 255, 255, 0.15)'"
+          type="submit"
+          variant="primary"
+          class="mr-1"
+        >
+          Submit
+        </b-button>
+        <b-button
+          v-ripple.400="'rgba(186, 191, 199, 0.15)'"
+          type="reset"
+          variant="outline-secondary"
+        >
+          Reset
+        </b-button>
+      </b-col>
+    </b-row>
+  </b-form>
+      </b-card>
+    </div>
 </template>
 
+
 <script>
-import { FormWizard, TabContent } from 'vue-form-wizard'
-import vSelect from 'vue-select'
-import 'vue-form-wizard/dist/vue-form-wizard.min.css'
 import {
-  BRow,
-  BCol,
-  BFormGroup,
-  BFormInput,
+  BCard,
+  BRow, BCol, BFormGroup, BFormInput, BFormCheckbox, BForm, BButton,
+  BFormTextarea ,
+  BFormFile ,
 } from 'bootstrap-vue'
-import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
+import Ripple from 'vue-ripple-directive'
 
 export default {
   components: {
-    FormWizard,
-    TabContent,
+    BFormFile,
+    BCard,
     BRow,
     BCol,
     BFormGroup,
     BFormInput,
-    vSelect,
-    // eslint-disable-next-line vue/no-unused-components
-    ToastificationContent,
+    BFormCheckbox,
+    BForm,
+    BButton,
+    BFormTextarea 
   },
-  data() {
-    return {
-      selectedContry: 'select_value',
-      selectedLanguage: 'nothing_selected',
-      countryName: [
-        { value: 'select_value', text: 'Select Value' },
-        { value: 'Russia', text: 'Russia' },
-        { value: 'Canada', text: 'Canada' },
-        { value: 'China', text: 'China' },
-        { value: 'United States', text: 'United States' },
-        { value: 'Brazil', text: 'Brazil' },
-        { value: 'Australia', text: 'Australia' },
-        { value: 'India', text: 'India' },
-      ],
-      languageName: [
-        { value: 'nothing_selected', text: 'Nothing Selected' },
-        { value: 'English', text: 'English' },
-        { value: 'Chinese', text: 'Mandarin Chinese' },
-        { value: 'Hindi', text: 'Hindi' },
-        { value: 'Spanish', text: 'Spanish' },
-        { value: 'Arabic', text: 'Arabic' },
-        { value: 'Malay', text: 'Malay' },
-        { value: 'Russian', text: 'Russian' },
-      ],
-    }
-  },
-  methods: {
-    formSubmitted() {
-      this.$toast({
-        component: ToastificationContent,
-        props: {
-          title: 'Form Submitted',
-          icon: 'EditIcon',
-          variant: 'success',
-        },
-      })
-    },
+  directives: {
+    Ripple,
   },
 }
 </script>
